7d6278d09a170e46402d2315869a179e17fa6360
Russell Davis <551404+russelldavis@users.noreply.github.com>
2023-10-07 12:22:55 -0700

    Use the workspace save command instead of the document.save function

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import { Uri } from 'vscode';
 
 async function saveAllExceptUntitled() {
   if (!await vscode.workspace.saveAll(/* includeUntitled */ false)) {
@@ -8,9 +7,13 @@ async function saveAllExceptUntitled() {
   // Always save the active document
   const doc = vscode.window.activeTextEditor?.document;
   if (doc && doc.isUntitled && doc.isDirty) {
-    if (!await doc.save()) {
-      vscode.window.showErrorMessage('Failed to active document');
-    }
+    // NB: Don't call doc.save() here. There's a weird bug where if the file doesn't exist yet
+    // on disk (e.g., from running `code some_new_file`), doc.save() will cause the editor to
+    // close after saving. After looking at the source, the full command also seems to handle some
+    // extra edge cases that are probably good to cover.
+    //
+    // Ignore return value, it will be false if the user is prompted for a filename and they cancel
+    await vscode.commands.executeCommand('workbench.action.files.save');
   }
 }
 
